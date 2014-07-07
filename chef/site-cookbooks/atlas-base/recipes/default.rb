ba65a38c6859432b05ced6b73ce7b033a85dced9
Diego Woitasen <diego@woitasen.com.ar>
2014-07-07 18:56:39 -0300

    Recipe extended to fix /etc/hosts.

--- chef/site-cookbooks/atlas-base/recipes/default.rb
+++ chef/site-cookbooks/atlas-base/recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+require 'socket'
+
 base_pkgs = [
   "ssh",
   "screen",
@@ -20,5 +22,34 @@ for pkg in base_pkgs do
   package pkg do
     action :install
   end
+end
+
+#Fix /etc/hosts
+domain = 'akilion.biz'
+hostname = Socket.gethostname
+first_ip = Socket.ip_address_list.detect { |ip| ip.ipv4? and !ip.ipv4_loopback? }
+ip = first_ip.getnameinfo[0]
+
+if hostname.include? "." do
+  raise "dots not allowed in hostnames: #{hostnames}"
+end
+
+file "/etc/hosts" do
+  owner "root"
+  group "root"
+  mode "0644"
+  contents "
+127.0.0.1 localhost.localdomain localhost
+#{ip}, #{hostname}.#{domain} #{hostname}
+
+# The following lines are desirable for IPv6 capable hosts
+::1     ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+
+"
+
 end
 
