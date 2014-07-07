9a47c9845c6f32e5a971378ee4e0d511bcbe2b21
Diego Woitasen <diego@woitasen.com.ar>
2014-07-06 21:33:22 -0300

    Use local knife configuration file if it exists.

--- chef/.chef/knife.rb
+++ chef/.chef/knife.rb
@@ -7,3 +7,8 @@ syntax_check_cache_path  '~/.chef/syntax_check_cache'
 #encrypted_data_bag_secret "data_bag_key"
 
 knife[:berkshelf_path] = "cookbooks"
+
+if ::File.exist?(File.expand_path("~/.chef/knife.atlas.local.rb", __FILE__))
+  Chef::Config.from_file(File.expand_path("~/.chef/knife.atlas.local.rb", __FILE__))
+end
+
