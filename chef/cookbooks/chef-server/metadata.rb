4380f1a83ba7fd14bd433d04e9c3c993b057e315
Diego Woitasen <diego@woitasen.com.ar>
2014-07-09 15:20:02 -0400

    Use the chef-server community cookbook.

---
+++ chef/cookbooks/chef-server/metadata.rb
@@ -0,0 +1,13 @@
+name              'chef-server'
+maintainer        'Opscode, Inc.'
+maintainer_email  'cookbooks@opscode.com'
+license           'Apache 2.0'
+description       'Installs and configures Chef Server'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '2.1.6'
+
+supports 'centos'
+supports 'amazon'
+supports 'ubuntu'
+
+# depends 'git'
