05cc947e42743a612a29109c752486c07045428d
Diego Woitasen <diego@woitasen.com.ar>
2014-07-13 19:55:06 -0300

    Setup region controller URL and maas-dns.

--- chef/site-cookbooks/maas-cluster-controller/recipes/default.rb
+++ chef/site-cookbooks/maas-cluster-controller/recipes/default.rb
@@ -10,12 +10,22 @@
 pkgs = [
   "python-seamicroclient",
   "python-pexpect",
-  "maas-cluster-controller"
+  "maas-cluster-controller",
+  "maas-dns"
 ]
 
 for pkg in pkgs do
   package pkg do
     action :install
+  end
+end
+
+ruby_block "set_maas_region_server" do
+  block do
+    file = Chef::Util::FileEdit.new("/etc/maas/maas_cluster.conf")
+    file.search_file_replace_line('^MAAS_URL=.*',
+                                  'MAAS_URL="http://107.170.185.50/MAAS"')
+    file.write_file
   end
 end
 
