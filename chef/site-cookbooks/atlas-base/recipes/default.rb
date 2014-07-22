ce1e3c5abf05c8015a02fa3f98e0de2edd8184bf
Diego Woitasen <diego@woitasen.com.ar>
2014-07-22 16:08:45 -0400

    Fix hosts operation removed. review it later.

--- chef/site-cookbooks/atlas-base/recipes/default.rb
+++ chef/site-cookbooks/atlas-base/recipes/default.rb
@@ -24,35 +24,3 @@ for pkg in base_pkgs do
   end
 end
 
-#Fix /etc/hosts
-domain = 'woitasen.com.ar'
-hostname = Socket.gethostname
-first_ip = Socket.ip_address_list.detect { |ip| ip.ipv4? and !ip.ipv4_loopback? }
-ip = first_ip.getnameinfo[0]
-
-if hostname.include? "."
-  raise "dots not allowed in hostnames: #{hostnames}"
-end
-
-if not ip
-  raise "IP address could not be discovered"
-end
-
-file "/etc/hosts" do
-  owner "root"
-  group "root"
-  mode "0644"
-  action :create
-  content "
-127.0.0.1 localhost.localdomain localhost
-#{ip} #{hostname}.#{domain} #{hostname}
-
-# The following lines are desirable for IPv6 capable hosts
-::1     ip6-localhost ip6-loopback
-fe00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-  "
-end
-
