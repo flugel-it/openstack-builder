75425c060e0d6535f99cf42b305e906866b2266b
Diego Woitasen <diego@woitasen.com.ar>
2014-09-24 13:37:42 -0400

    post install script sets the hostname now.

--- scripts/build-post-install.sh
+++ scripts/build-post-install.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
 
-wget -O /usr/local/bin/install-salt.sh \
+setup_hostname(){
+
+	CURRHOSTNAME=`cat /etc/hostname | awk -F'.' '{print $1}'`
+	MAC=$(ip a s dev eth0 | grep link\/eth | awk '{print $2}' | awk -F\: '{print $4$5$6}')
+
+	if ! grep ${MAC} /etc/hostname; then
+
+		NEWHOSTNAME=${CURRHOSTNAME}-$MAC
+		echo ${NEWHOSTNAME} > /etc/hostname
+
+		if [ -f /etc/salt/minion_id ]; then
+			echo ${NEWHOSTNAME} > /etc/salt/minion_id
+			stop salt-minion
+			start salt-minion
+		fi
+
+		reboot
+
+	fi
+
+}
+
+setup_hostname &&
+wget -O install_salt.sh https://bootstrap.saltstack.com &&
+sh install_salt.sh -i $HOSTNAME -A cloud-master.flugel.it git v2014.7.0rc2 &&
+exit 0
+
+exit 1
 
