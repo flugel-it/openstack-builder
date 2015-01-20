ffb2f70b322e20b8a31d34f456b703c2b32fe853
Luis <luis@flugel.it>
2015-01-20 11:26:03 -0300

    install-controller.sh

--- scripts/install-controller.sh
+++ scripts/install-controller.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # You can optionally connect to the VM with
 # virt-viewer -c qemu:///system Debian7
 
@@ -8,18 +8,21 @@
 #      install-controller.sh [kvm_server]
 #
 
-export OS=Ubuntu-14.04
-
 DEST=$1
+OS=$2
+
+[ "$OS" == "" ] && OS=Ubuntu-14.04
+
+#--file /var/lib/libvirt/images/openstack_controller.img \
 
 virt-install \
 --connect qemu+ssh://root@${DEST}/system \
 --name ${OS} \
 --ram 512 \
 --vcpus 1 \
---file /var/lib/libvirt/images/FileName.img \
+--file /var/lib/libvirt/images/openstack_controller.img \
 --file-size=10 \
---location http://mirrors.usc.edu/pub/linux/distributions/ubuntu/dists/trusty/main/installer-amd64/ \
+--location http://us.archive.ubuntu.com/ubuntu/dists/trusty/main/installer-amd64/ \
 --virt-type kvm \
 --os-variant ubuntutrusty \
 --network bridge=virbr0 \
